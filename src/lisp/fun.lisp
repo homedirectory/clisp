ec6985239eca1912bc17646e73d7db0bf669ef08
homedirectory <47361170+homedirectory@users.noreply.github.com>
2023-05-15 11:41:55 +0300

    fun.lisp: adjust to updated let* form

--- src/lisp/fun.lisp
+++ src/lisp/fun.lisp
@@ -5,8 +5,8 @@
              (sum-acc (- x 1) (+ x acc)))))
 
 (def! collatz
-      (let* (cltz (fn* (m nums)
-                       (cond ((<= m 1)  (cons m nums))
-                             ((even? m) (cltz (/ m 2) (cons m nums)))
-                             (true      (cltz (+ 1 (* 3 m)) (cons m nums))))))
+      (let* ((cltz (fn* (m nums)
+                        (cond ((<= m 1)  (cons m nums))
+                              ((even? m) (cltz (/ m 2) (cons m nums)))
+                              (true      (cltz (+ 1 (* 3 m)) (cons m nums)))))))
         (fn* (n) (cltz n '()))))
